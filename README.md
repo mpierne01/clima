c864dd05c8725ac88e49791a943a2a2596f910b9
Darkhammer12 <Darkhammer12@protonmail.com>
2019-01-06 18:48:11 -0800

    TODO: Set LOCATION_PROVIDER

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # clima
 
+//If you go to compile and you get a cross toolchain error You must uncomment out the imports. This is an error on Android Studio. Not the build. 
 ## TODO Layout
 - Weather Controller
 - WeatherDataModel
+
+## Weather Controler
+- TODO: public class WeatherController extends AppCompatActivity
+- TODO: Set loopj dependancy
+- TODO: App Weather ID
+- TODO
+Set LOCATION_PROVIDER
--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -1,22 +1,54 @@
-package com.londonappbrewery.climapm;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+
+
+
 
 
 public class WeatherController extends AppCompatActivity {
 
-    // Constants:
+    // Request Codes:
+    final int REQUEST_CODE = 123; // Request Code for permission request callback
+    final int NEW_CITY_CODE = 456; // Request code for starting new activity for result callback
+
+    // Base URL for the OpenWeatherMap API. More secure https is a premium feature =(
     final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
-    // App ID to use OpenWeather data
-    final String APP_ID = "e72____PLEASE_REPLACE_ME_____13";
+
+    // App ID to use OpenWeather data  MUST ADD YOUR OWN TO WORK
+    final String APP_ID = "183884856c01c37b847fccdcbf90d6e3";
+
     // Time between location updates (5000 milliseconds or 5 seconds)
     final long MIN_TIME = 5000;
+
     // Distance between location updates (1000m or 1km)
     final float MIN_DISTANCE = 1000;
+
+    // Don't want to type 'Clima' in all the logs, so putting this in a constant here.
+    final String LOGCAT_TAG = "Clima";
 
     // TODO: Set LOCATION_PROVIDER here:
 
@@ -24,11 +56,14 @@ public class WeatherController extends AppCompatActivity {
 
 
     // Member Variables:
+    boolean mUseLocation = true;
     TextView mCityLabel;
     ImageView mWeatherImage;
     TextView mTemperatureLabel;
 
-    // TODO: Declare a LocationManager and a LocationListener here:
+    // Declaring a LocationManager and a LocationListener here:
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
 
 
     @Override
@@ -36,11 +71,15 @@ public class WeatherController extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_controller_layout);
 
-        // Linking the elements in the layout to Java code
-        mCityLabel = (TextView) findViewById(R.id.locationTV);
-        mWeatherImage = (ImageView) findViewById(R.id.weatherSymbolIV);
-        mTemperatureLabel = (TextView) findViewById(R.id.tempTV);
-        ImageButton changeCityButton = (ImageButton) findViewById(R.id.changeCityButton);
+        // Linking the elements in the layout to Java code.
+        // API 26 and above does not require casting anymore.
+        // Can write: mCityLabel = findViewById(R.id.locationTV);
+        // Instead of: mCityLabel = (TextView) findViewById(R.id.locationTV);
+
+        mCityLabel = findViewById(R.id.locationTV);
+        mWeatherImage = findViewById(R.id.weatherSymbolIV);
+        mTemperatureLabel = findViewById(R.id.tempTV);
+        ImageButton changeCityButton = findViewById(R.id.changeCityButton);
 
 
 
